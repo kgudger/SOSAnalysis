8e2673860fbaa65aaad186ceae0eab9cbe29f75e
Keith Gudger <kgudger@gmail.com>
2018-08-11 17:24:00 -0700

    Added new file with better data handling

--- SOS2.R
+++ SOS2.R
@@ -1,19 +1,66 @@
-b2017data <- read.csv("../SOSdata/2017-working-transpose.csv")
-b2017adata <- read.csv("../SOSdata/2017acc-working-transpose.csv")
-b2016data <- read.csv("../SOSdata/2016-working-transpose.csv")
-b2016adata <- read.csv("../SOSdata/2016acc-working-transpose.csv")
-b2015data <- read.csv("../SOSdata/2015-working-transpose.csv")
-b2014data <- read.csv("../SOSdata/2014-working-transpose.csv")
-b2013data <- read.csv("../SOSdata/2013-working-transpose.csv")
-b2012data <- read.csv("../SOSdata/2012-working-transpose.csv")
-b2011data <- read.csv("../SOSdata/2011-working-transpose.csv")
-b2010data <- read.csv("../SOSdata/2010-working-transpose.csv")
-b2009data <- read.csv("../SOSdata/2009-working-transpose.csv")
-b2008data <- read.csv("../SOSdata/2008-working-transpose.csv")
+read_rename <- function(csv) {
+  df <- read.csv(csv)
+  colnames(df)[agrep("Cigarette.Butts",colnames(df))] <- 
+    "Cigarette.Butts"
+  colnames(df)[agrep("Plastic.Bags.Grocery",colnames(df))] <- 
+    "Plastic.Bags.Grocery"
+  colnames(df)[agrep("Plastic.straws.stirrers",colnames(df))] <- 
+    "Plastic.straws.stirrers"
+  colnames(df)[agrep("Syringes",colnames(df))] <- 
+    "Syringes.or.Needles"
+  colnames(df)[agrep("Cardboard",colnames(df))] <- 
+    "Cardboard"
+  colnames(df)[agrep("Tires",colnames(df))] <- 
+    "Tires"
+  colnames(df)[agrep("id20",colnames(df))] <- 
+    "id"
+  colnames(df)[agrep("Metal.Cans",colnames(df))] <- 
+    "Metal.Cans"
+  colnames(df)[agrep("Metal.Bottle.Caps",colnames(df))] <- 
+    "Metal.Bottle.Caps"
+  colnames(df)[agrep("Metal.Can.Pulls",colnames(df))] <- 
+    "Metal.Can.Pulls"
+  colnames(df)[agrep("Plastic.Bottle.Caps",colnames(df))] <- 
+    "Plastic.Bottle.Caps"
+  colnames(df)[grep("Plastic.Bottles",colnames(df),ignore.case=TRUE)] <-
+    "Plastic.Bottles"
+  colnames(df)[grep("Glass.Bottles",colnames(df),ignore.case=TRUE)] <-
+    "Glass.Bottles"
+  colnames(df)[agrep("Fireworks",colnames(df))] <- 
+    "Fireworks"
+  colnames(df)[grep("Styrofoam.Cups|Styrofoam.food.containers|Polystyrene.food..to.go|Polystyrene.cups",colnames(df))] <- 
+    "Styrofoam.Cups.Plates.Bowls"
+  colnames(df)[grep("Styrofoam.Pieces|Pieces..Styrofoam",colnames(df),ignore.case=TRUE)] <-
+    "Styrofoam.Pieces"
+  colnames(df)[agrep("Cigarette.Lighters",colnames(df))] <- 
+    "Cigarette.Lighters"
+  colnames(df)[grep("Metal.Fishing.hooks|Lures.or.Hooks",colnames(df),ignore.case=TRUE)] <-
+    "Metal.Fishing.Hooks.Lures"
+  colnames(df)[grep("Condoms",colnames(df),ignore.case=TRUE)] <-
+    "Condoms"
+  colnames(df)[grep("Diapers",colnames(df),ignore.case=TRUE)] <-
+    "Diapers"
+  colnames(df)[grep("Pallets",colnames(df),ignore.case=TRUE)] <-
+    "Pallets"
+  return(df)
+}
+b2017data <- read_rename("../SOSdata/2017-working-transpose.csv")
+b2017adata <- read_rename("../SOSdata/2017acc-working-transpose.csv")
+b2016data <- read_rename("../SOSdata/2016-working-transpose.csv")
+b2016adata <- read_rename("../SOSdata/2016acc-working-transpose.csv")
+b2015data <- read_rename("../SOSdata/2015-working-transpose.csv")
+b2014data <- read_rename("../SOSdata/2014-working-transpose.csv")
+b2013data <- read_rename("../SOSdata/2013-working-transpose.csv")
+b2012data <- read_rename("../SOSdata/2012-working-transpose.csv")
+b2011data <- read_rename("../SOSdata/2011-working-transpose.csv")
+b2010data <- read_rename("../SOSdata/2010-working-transpose.csv")
+b2009data <- read_rename("../SOSdata/2009-working-transpose.csv")
+b2008data <- read_rename("../SOSdata/2008-working-transpose.csv")
 list.of.data.frames = list(b2017data, b2017adata, b2016data, b2016adata, b2015data, b2014data, b2013data, b2012data, b2011data, b2010data, b2009data, b2008data)
-merged.data.frame = Reduce(function(...) merge(..., all=T), list.of.data.frames)
-#tail(merged.data.frame)
 library(plyr)
+merged.data.frame <- join_all(list.of.data.frames,type='full')
+#merged.data.frame = Reduce(function(...) merge(..., all=T), list.of.data.frames)
+#tail(merged.data.frame)
 # clean up data, remove NAs
 merged.data.frame$Pounds.of.Trash.Collected <- as.numeric(as.character(merged.data.frame$Pounds.of.Trash.Collected))
 ind <- which(sapply(merged.data.frame, is.numeric))
@@ -21,61 +68,6 @@ for(j in ind){
   merged.data.frame[[j]] <- as.numeric(as.character(merged.data.frame[[j]]))
 #  merged.data.frame[[j]] <- replace(merged.data.frame[[j]],is.na(merged.data.frame[[j]]),0)
 }
-# combine all cigarette butts into one column
-merged.data.frame$Cigarette.Butts.Total = merged.data.frame$Cigarette.Butts + merged.data.frame$Cigarette.butts
-# delete old columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Cigarette.Butts,Cigarette.butts))
-# combine all definite plastic grocery bags into one column
-merged.data.frame$Plastic.Grocery.Total = merged.data.frame$Plastic.Bags..grocery..shopping. + merged.data.frame$Grocery.Bags..Plastic.
-# delete old plastic grocery bags columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Plastic.Bags..grocery..shopping.,Grocery.Bags..Plastic.))
-# combine all plastic straws into one column
-merged.data.frame$Plastic.Straws.Total = merged.data.frame$Plastic.straws.or.stirrers + merged.data.frame$Straws.Stirrers + merged.data.frame$Plastic.straws.stirrers
-# delete old plastic straws columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Plastic.straws.or.stirrers,Straws.Stirrers,Plastic.straws.stirrers))
-# combine all syringes into one column
-merged.data.frame$Syringes.Total = merged.data.frame$Syringes + merged.data.frame$Syringes.or.needles + merged.data.frame$Hazardous.Syringes.Needles
-# delete old syringes columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Syringes,Syringes.or.needles,Hazardous.Syringes.Needles))
-# combine all Cardboard into one column
-merged.data.frame$Cardboard.Total = merged.data.frame$Cardboard + merged.data.frame$Paper.Cardboard
-# delete old Carboard columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Cardboard,Paper.Cardboard))
-# combine all Tires into one column
-merged.data.frame$Tires.Total = merged.data.frame$Tires + merged.data.frame$Tires.1
-# delete old Tires columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Tires,Tires.1))
-# combine all id columns
-merged.data.frame$id = merged.data.frame$id2008 + merged.data.frame$id2009 +
-  merged.data.frame$id2010 + merged.data.frame$id2011 + merged.data.frame$id2012 + 
-  merged.data.frame$id2013 + merged.data.frame$id2014 + merged.data.frame$id2015 + 
-  merged.data.frame$id2016 + merged.data.frame$id2016b + merged.data.frame$id2017
-# delete extra id columns
-merged.data.frame <- subset(merged.data.frame, select= -c(id2008,id2009,id2010,id2011,id2012,id2013,id2014,id2015,id2016,id2017,id2016b))
-# combine all metal beverage cans into one column
-merged.data.frame$Metal.Drink.Cans.Total = merged.data.frame$Metal.beverage.cans + merged.data.frame$Metal.Beer.Cans + merged.data.frame$Metal.Soda.cans
-# delete old Metal Drink Cans columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Metal.beverage.cans,Metal.Beer.Cans,Metal.Soda.cans))
-# combine all metal can tops and tabs into one column
-merged.data.frame$Metal.Caps.Tabs.Total = merged.data.frame$Metal.bottle.caps.or.can.pulls + merged.data.frame$Bottle.Caps..Metal. + merged.data.frame$Metal.Bottle.caps + merged.data.frame$Metal.Bottle.Caps.or.Pull.Tabs
-# delete old Metal Cans Tops and Tabs columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Metal.bottle.caps.or.can.pulls,Bottle.Caps..Metal.,Metal.Bottle.caps,Metal.Bottle.Caps.or.Pull.Tabs))
-# combine all plastic tops and rings into one column
-merged.data.frame$Plastic.Caps.Rings.Total = merged.data.frame$Plastic.bottle.caps.or.rings + merged.data.frame$Bottle.Caps..Plastic. + merged.data.frame$Plastic.Bottle.Caps.Rings
-# delete old Plastic Tops and rings columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Plastic.bottle.caps.or.rings,Bottle.Caps..Plastic.,Plastic.Bottle.Caps.Rings))
-# combine all plastic bottles into one column
-merged.data.frame$Plastic.Bottles.Total = merged.data.frame$Plastic.bottles + merged.data.frame$Plastic.Bottles
-# delete old Plastic bottles columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Plastic.bottles,Plastic.Bottles))
-# combine all glass bottles into one column
-merged.data.frame$Glass.Bottles.Total = merged.data.frame$Glass.bottles + merged.data.frame$Glass.Bottles
-# delete old Glass bottles columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Glass.bottles,Glass.Bottles))
-# combine all Fireworks into one column
-merged.data.frame$Fireworks = merged.data.frame$Fireworks + merged.data.frame$Fireworkds
-# delete Fireworkds columns
-merged.data.frame <- subset(merged.data.frame, select= -c(Fireworkds))
 # cleanup Cleanup.Site de-dup
 merged.data.frame$Cleanup.Site[agrep("Davenport",merged.data.frame$Cleanup.Site)]<-"Davenport Main Beach"
 merged.data.frame$Cleanup.Site[agrep("Seacliff",merged.data.frame$Cleanup.Site)]<-"Seacliff State Beach"
